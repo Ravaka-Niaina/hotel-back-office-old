a1e20565d9147f39e04b10bd2fecb7ad4e7d7107
Adriela <adriela_ratefiarivony@hotmail.fr>
2021-11-05 13:43:54 +0300

    begin calendar

--- src/tarif/Tarif.js
+++ src/tarif/Tarif.js
@@ -1,5 +1,5 @@
 import './Tarif.css';
-import * as React from 'react';
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -8,7 +8,17 @@ import Box from '@mui/material/Box';
 import  Navbar  from "../Navbar/Navbar";
 import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
 import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
+import axios from "axios";
+import Button from '@mui/material/Button';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CalendarComponent from './CalendarComponent'
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -42,6 +52,52 @@ function a11yProps(index) {
   };
 }
 
+function ListTarif(){
+    const [tarifs, setTarifs] = useState([]);
+    axios({
+        method: 'get',
+        url: process.env.REACT_APP_BACK_URL + 
+            "/tarif",
+        withCredentials: true
+    })
+    .then(res => setTarifs(res.data.list))
+    .catch(err => console.log(err));
+    let list = null;
+    list = tarifs.map(tarif => {
+        return <TableRow
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+            <TableCell align="center">{tarif.nom}</TableCell>
+            <TableCell align="center">{tarif.prixParJour}</TableCell>
+            <TableCell align="center">{tarif.services}</TableCell>
+            <TableCell align="center">{tarif.conditionsAnnulation}</TableCell>
+            <TableCell align="center">
+            <Button variant="contained" style={{backgroundColor:'#4682B4'}}>
+                Modifier
+            </Button>
+            </TableCell>
+        </TableRow>
+    });
+    return(
+        <TableContainer component={Paper} style={{marginTop:'40px'}}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell align="center"><strong>Nom</strong></TableCell>
+                <TableCell align="center"><strong>Prix par jour</strong></TableCell>
+                <TableCell align="center"><strong>Services</strong></TableCell>
+                <TableCell align="center"><strong>Conditions d'annulation</strong></TableCell>
+                <TableCell align="center"><strong>Actions</strong></TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            { list }
+            </TableBody>
+        </Table>
+        </TableContainer>
+    );
+}
+
 function Tarif() {
     const [value, setValue] = React.useState(0);
     const handleChange = (event, newValue) => {
@@ -58,10 +114,10 @@ function Tarif() {
                     </Tabs>
                 </Box>
                 <TabPanel value={value} index={0}>
-                    Item One
+                    <ListTarif/>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    Item Two
+                    <CalendarComponent/>
                 </TabPanel>
             </Box>
             </>
