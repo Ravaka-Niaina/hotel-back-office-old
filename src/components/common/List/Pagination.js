f6b8ab064bcad03bab611c9694e9cf36f7fa9271
Anja <ratefyniaina@gmail.com>
2022-01-18 09:49:11 +0300

    merge historique

--- src/components/common/List/Pagination.js
+++ src/components/common/List/Pagination.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/components/common/List/Pagination.js
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import PaginationItem from '@mui/material/PaginationItem';
@@ -16,23 +15,4 @@ export default function Pagine(props) {
       />
     </Stack>
   );
-=======
-import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
-import Stack from '@mui/material/Stack';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-
-export default function Pagine(props) {
-  return (
-    <Stack spacing={2}>
-      <Pagination
-        page={props.currentNumPage} 
-        count={props.pagine}
-        onChange={(e, val) => {props.setCurrentNumPage(val); props.rechercher(val)}}
-      />
-    </Stack>
-  );
->>>>>>> origin/ListHistoriqueAnja:src/components/common/List/List/Pagination.js
 }
