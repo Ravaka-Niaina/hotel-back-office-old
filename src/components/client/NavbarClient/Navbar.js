50cf80bee3c4cce20b36bf6d893bf0bd452c3bcf
Norck <ratefyniaina@gmail.com>
2022-02-10 12:42:31 +0300

    ch1

--- src/components/client/NavbarClient/Navbar.js
+++ src/components/client/NavbarClient/Navbar.js
@@ -177,13 +177,6 @@ function Navbar(props) {
                                 <span style={{color:'black'}}>S'inscrire</span>
                             </Button>
                            </div>
-                           <div class="mdp">
-                           <Link to={'/login/identify'} style={{textDecoration:'none'}}>
-                            <Button  id="mdp">
-                            <span style={{color:'black'}}>Mot de passe oublié?</span>
-                            </Button>
-                            </Link>
-                            </div>
                           </div>
                           </Box>
                             
