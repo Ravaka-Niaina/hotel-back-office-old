210c0b8319118afd1be9b1040cbc84a3be04b570
tokiniainaherve.andrianarison <tokiniainaherve.andrianarison@gmail.com>
2022-03-19 17:10:06 +0300

    template

--- src/components/partenaire/publipostage/ModeleEmail.js
+++ src/components/partenaire/publipostage/ModeleEmail.js
@@ -12,7 +12,7 @@ function ModeleEmail(props){
         
         <>
           
-          <div class="container" style={{fontFamily:'Roboto,RobotoDraft,Helvetica,Arial,sans-serif',width:800,margin:'0 auto',padding:'0 auto',marginTop:50}}>
+          <div class="container" style={{fontFamily:'Roboto,RobotoDraft,Helvetica,Arial,sans-serif',width:800,margin:'0 auto',padding:'0 auto',marginTop:80}}>
                 <div class='itineraire' >  
                         
                         <div class='header'  style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}} >
@@ -38,24 +38,24 @@ function ModeleEmail(props){
                       <div class='reservations' >
                         <div class='reservations_details' style={{marginTop: '1rem',border: '1px solid black'}}>
                             <table style={{marginLeft:'auto',marginRight:'auto',width: '97%'}}>
-                                <tr style={{height:15,borderBottom:'1px solid',borderColor:'#f7f7f7'}}>
+                                <tr style={{height:15,borderBottom:'1px solid',borderColor:'#f7f7f7',verticalAlign:'baseline'}}>
                                     <td style={{fontFamily:'arial,helvetica,sans-serif',fontSize:12,color:'#333333',width:187.5,height:15,textAlign:'center'}} colspan='3'><span style={{fontSize:10}}><span style={{fontFamily:'arial,helvetica,sans-serif'}}><strong style={{fontStyle:'normal',color:'#333333'}}><strong>Clients :</strong></strong></span></span><br/><span style={{fontSize:10}}> X Adulte(s) X Enfant(s)<br/><br/></span></td>
-                                    <td style={{width:190,height:15,textAlign:'center'}}><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Date d'arrivée :</strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>mardi 08 mars 2022</span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Check-in : 12.00 AM<br/><br/></strong></span></td><br/>
+                                    <td style={{width:190,height:15,textAlign:'center',paddingLeft:100}}><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Date d'arrivée :</strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>mardi 08 mars 2022</span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Check-in : 12.00 AM<br/><br/></strong></span></td><br/>
                                     <td style={{width:202.5,height:15,textAlign:'center'}} colspan='2'><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Date depart :</strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>mardi 08 mars 2022</span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Check-out : 12.00 AM<br/><br/></strong></span></td><br/>
                                 
                                 </tr>
-                                <tr style={{height:15,borderBottom:'1px solid',borderColor:'#f7f7f7'}}>
-                                    <td style={{fontFamily:'arial,helvetica,sans-serif;fontSize:12',color:'#333333',width:187.5,height:15,textAlign:'center'}} colspan='3'><span style={{fontSize:10}}><span style={{fontFamily:'arial,helvetica,sans-serif'}}><strong style={{fontStyle:'normal',color:'#333333'}}><strong>Nombre de nuit:</strong></strong></span></span><br/><span style={{fontSize:10}}>1 <br/><br/></span></td>
-                                    <td style={{width:190,height:15,textAlign:'center'}}><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Type de Chambre Réservé</strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>Chambre standard</span><br/></td><br/>
+                                <tr style={{height:15,borderBottom:'1px solid',borderColor:'#f7f7f7',verticalAlign:'baseline'}}>
+                                    <td style={{fontFamily:'arial,helvetica,sans-serif',fontSize:12,color:'#333333',width:187.5,height:15,textAlign:'center'}} colspan='3'><span style={{fontSize:10}}><span style={{fontFamily:'arial,helvetica,sans-serif'}}><strong style={{fontStyle:'normal',color:'#333333'}}><strong>Nombre de nuit:</strong></strong></span></span><br/><span style={{fontSize:10}}>1 <br/><br/></span></td>
+                                    <td style={{width:190,height:15,textAlign:'center',paddingLeft:100}}><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Type de Chambre Réservé</strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>Chambre standard</span><br/></td><br/>
                                     <td style={{width:202.5,height:15,textAlign:'center'}} colspan='2'><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}><strong>Plan Tarifaire: </strong></span><br/><span style={{fontFamily:'arial,helvetica,sans-serif',fontSize:10}}>Petit déjeuner - Non remboursable</span><br/></td><br/>
                                 
                                 </tr>
-                                <tr style={{marginTop: '1rem' ,height:15,backgroundColor:'#f7f7f7'}}>
+                                <tr style={{marginTop: '1rem' ,height:15,backgroundColor:'#f7f7f7',verticalAlign:'baseline'}}>
                                     <td style={{fontFamily:'arial,helvetica,sans-serif',fontSize:12,color:'#333333',height:15,width:605}} colspan='6'><span style={{fontFamily:'arial,helvetica,sans-serif'}}><strong style={{fontStyle:'normal',color:'#333333',fontSize:12}}><br/>Détails et préférences: <br/></strong></span>
                                             messageParticulier
                                     </td>
                                 </tr>
-                                <tr style={{marginTop: '1rem' ,height:15,backgroundColor:'#f7f7f7'}}>
+                                <tr style={{marginTop: '1rem' ,height:15,backgroundColor:'#f7f7f7',verticalAlign:'baseline'}}>
                                     <td style={{fontFamily:'arial,helvetica,sans-serif',fontSize:12,color:'#333333',height:15,width:605}} colspan='6'><span style={{fontFamily:'arial,helvetica,sans-serif'}}><strong style={{fontStyle:'normal',color:'#333333',fontSize:12}}><br/>Détails et préférences: <br/></strong></span>
                                         <p>Politiques 1</p>
                                         <p>Politiques 2</p>
@@ -74,7 +74,7 @@ function ModeleEmail(props){
                         </div>
                       </div>
                       <div style={{fontFamily:'arial,helvetica,sans-serif',marginTop: '1rem',marginLeft:'1rem',fontSize:12,color:'#333333',width:588.008}}>
-                        <p><span style={{fontSize:10}}>Pour toute question concernant votre réservation, veuillez appeler le département de réservation de "+ hotel.nom+" sur le [telephone de l'hotel] ou envoyer un email au <a href='"+hotel.email+"' target='_blank'>"+hotel.email+"</a><br/><br/></span>Salutations,<br/>[nom de l'hotel]<br/><br/></p>
+                        <p><span style={{fontSize:10}}>Pour toute question concernant votre réservation, veuillez appeler le département de réservation de "+ hotel.nom+" sur le [telephone de l'hotel] ou envoyer un email au <a href='"+hotel.email+"' target='_blank'>[email]</a><br/><br/></span>Salutations,<br/>[nom de l'hotel]<br/><br/></p>
                       </div>
                 </div>
           </div>
