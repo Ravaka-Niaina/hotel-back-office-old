0c51ee548c5f35737b3c7ebcc92747ac4cf98381
ratefiarivony ravaka niaina <ratefyniaina@gmail.com>
2022-06-22 17:09:40 +0300

    chore: add loading circle to status day div

--- src/components/partenaire/Calendrier/tarif/dependancies/CloseLine.js
+++ src/components/partenaire/Calendrier/tarif/dependancies/CloseLine.js
@@ -1,11 +1,23 @@
 import styles from '../CalendarComponent.module.css';
 import {Box} from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useState } from 'react';
 import callAPI from '../../../../../utility';
 const utility = require('../utility.js');
 
+const LoadingButton = () => {
+  return (
+    <div className={styles.loadingButton}>
+      <CircularProgress size={15} color="inherit" />
+    </div>
+  );
+}
+
 const CloseLine = (props) => {
     const { typeChambres, setTypeChambres, indice, indexStatus, } = props;
+    const [isLoading, setIsLoading] = useState(false);
+
 
     const theme = createTheme({
         palette: {
@@ -17,17 +29,17 @@ const CloseLine = (props) => {
     })
 
     function callbackCloseTypeChambre(res){
-        console.log(res);
         if(res.status === 200){
             props.setOpenLoad(false);
             const statusDay = typeChambres[indice].statusDays[indexStatus];
             statusDay.closed = !statusDay.closed;
             setTypeChambres([ ...typeChambres ]);
         }
+        setIsLoading(false);
     }
 
     function closeTypeChambre(){
-        // props.setOpenLoad(true);
+        setIsLoading(true);
         const data = {
             _id: props.idTypeChambre, 
             dateDebut: utility.getDate(props.statusDay.date),
@@ -39,9 +51,12 @@ const CloseLine = (props) => {
     return (
         <>
           <ThemeProvider
-              theme={theme}
-              >
-              <Box
+            theme={theme}
+          >
+              { 
+                isLoading
+                ? <LoadingButton />
+                : <Box
                   className={styles.closedline}
                   sx={{
                   width: 59,
@@ -52,8 +67,9 @@ const CloseLine = (props) => {
                   position: 'relative',
                   }}
                   onClick={closeTypeChambre}
-              >
-              </Box>
+                >
+                </Box>
+              }
           </ThemeProvider>
         </>
     );
