cd81f2da6f3a4f08126fb6b7211b42fb276d7e07
Ravaka-Niaina <103428442+Ravaka-Niaina@users.noreply.github.com>
2022-07-14 13:09:50 +0300

    fix: rapport reservation ne s'affichait plus sur la page d'accueil back

--- src/home/home.js
+++ src/home/home.js
@@ -11,92 +11,22 @@ import Login from '../components/common/Authentification/Login.js';
 
 import ResponsiveDrawer from "../components/partenaire/Navbar/responsive-drawer.js";
 import DashboardLayout from '../layouts/dashboard';
+import RapportReservation from '../components/partenaire/rapportReservation/rapportReservation';
 // material
 import { Box, Grid, Typography } from '@mui/material';
-// components
-import Page from '../comp/Page';
-import {
-  AppTasks,
-  AppNewUsers,
-  AppBugReports,
-  AppItemOrders,
-  AppNewsUpdate,
-  AppWeeklySales,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppCurrentSubject,
-  AppConversionRates
-} from '../sections/@dashboard/app';
 
 function home() {
     return (
-        // <>
-        // {/* <Navbar currentPage={0}/> */}
-        // <Container className="mainContainer">
-        //     <h1 style={{margin: '0 auto'}}>Bienvenue!</h1>
-        // </Container>
-        // </>
-
-        // <Page title="Dashboard | Minimal-UI">
-        // <>
-        // <DashboardLayout />
         <Container maxWidth="xl">
             <Box sx={{ pb: 5 }}>
             <Typography variant="h4">Hi, Welcome back</Typography>
             </Box>
             <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} md={3}>
-                <AppWeeklySales />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <AppNewUsers />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <AppItemOrders />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <AppBugReports />
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={8}>
-                <AppWebsiteVisits />
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={4}>
-                <AppCurrentVisits />
-            </Grid>
-
-            {/* <Grid item xs={12} md={6} lg={8}>
-                <AppConversionRates />
-            </Grid> */}
-
-            {/* <Grid item xs={12} md={6} lg={4}>
-                <AppCurrentSubject />
-            </Grid> */}
-
-            {/* <Grid item xs={12} md={6} lg={8}>
-                <AppNewsUpdate />
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={4}>
-                <AppOrderTimeline />
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={4}>
-                <AppTrafficBySite />
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={8}>
-                <AppTasks />
-            </Grid> */}
+              <RapportReservation />
             </Grid>
         </Container>
-        //  </Page>
-        // </>
-        );
-    }
+    );
+}
   
 export default function home_() {
     return(
